d8cf344cedd792a50a18cc82d2f52a777b22318e
Siftab <siiffuuu@gmail.com>
2024-09-25 12:41:25 +0600

    initializing

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1 className="text-3xl">React With Redux</h1>
+   </div>
+  )
+}
+
+export default App
