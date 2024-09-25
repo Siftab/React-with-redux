a2301e56122464dd39191e7c1cbed877dd8ad488
Siftab <siiffuuu@gmail.com>
2024-09-25 13:06:14 +0600

    add some code   practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [count,setCount]=useState(0)
 
   return (
    <div>
-    <h1 className="text-3xl">React With Redux</h1>
+
+   <div className="flex flex-col justify-center items-center border border-indigo-600 h-screen space-y-10">
+   <h1 className="text-3xl">React With Redux</h1>
+ 
+   <div className="flex space-x-9">
+   <button className="px-4 rounded-md text-white py-3 bg-green-500 font-semibold">increment</button>
+    <p className=" text-3xl">{count}</p>
+    <button className="px-4 rounded-md text-white py-3 bg-red-500 font-semibold">decrement</button>
+   </div>
+   </div>
    </div>
   )
 }
